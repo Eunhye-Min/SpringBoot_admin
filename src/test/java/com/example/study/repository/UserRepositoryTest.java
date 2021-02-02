6fbe08eebeb77dea77763ee246af843cd88f463c
EunhyeMin <olivgrece@gmail.com>
2021-02-03 00:03:44 +0900

    210203TIL

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -2,10 +2,15 @@ package com.example.study.repository;
 
 import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
     //Dependency Injection (DI)
@@ -27,15 +32,43 @@ public class UserRepositoryTest extends StudyApplicationTests {
         System.out.println("newUser : " + newUser);
     }
 
+    @Test
     public void read(){
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser ->{
+            System.out.println("user : " + selectUser );
+            System.out.println("email : " + selectUser.getEmail());
+        });
     }
 
+    @Test
     public void update(){
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser ->{
+            selectUser.setAccount("PPPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+
+        });
     }
 
+    @Test
+    @Transactional
     public void delete(){
+        Optional<User> user = userRepository.findById(1L);
 
+        Assertions.assertTrue(user.isPresent());
+
+        user.ifPresent(selectUser->{
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(1L);
+
+        Assertions.assertFalse(deleteUser.isPresent());
     }
 }
